133c1153f1eac0149a16432006718235a40c1726
Arivan7586 <1194028288@qq.com>
2018-12-10 22:49:42 +0800

    复习继承和覆写

--- javacode/Test12_10.java
+++ javacode/Test12_10.java
@@ -22,24 +22,38 @@ class Person{
     public int getAge(){
         return this.age;
     }
-
+    public static void say(){
+        System.out.println("我是一个人！");
+    }
 }
 
 class Worker extends Person{
     public Worker(){
         System.out.println("我是子类");
     }
+    public static void print(){
+        System.out.println("我是一名工人！");
+    }
+}
+
+class Student extends Worker{  //继承了Person类和Student类
+    public static void say(){  
+        System.out.println("我现在是一名学生！");  //方法的覆写
+    }
 }
 
 public class Test12_10{
     public static void main(String[] args){
-        Person per = new Person();
-        Worker w = new Worker();
-        w.setName("小黑");
-        w.setAge(25);
-        System.out.println(w.getName());
-        System.out.println(w.getAge());
+        // Person per = new Person();
+        // Worker w = new Worker();
+        // w.setName("小黑");
+        // w.setAge(25);
+        // System.out.println(w.getName());
+        // System.out.println(w.getAge());
 
-        System.out.println(w.country);      
+        // System.out.println(w.country);      
+
+        Student stu = new Student();
+        stu.say();
     }
 }
