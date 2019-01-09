97613558c95e9a27a6a97b79f960913d1768d98e
Arivan7586 <1194028288@qq.com>
2019-01-09 21:27:12 +0800

    修改小练习

--- javacode/src/www/arivan/Test1_8/Test1_8.java
+++ javacode/src/www/arivan/Test1_8/Test1_8.java
@@ -1,4 +1,5 @@
 package www.arivan.Test1_8;
+import java.util.Scanner;
 
 //2.题目描述：给定一个英文字符串，写一段代码找出字符串中首先出现三次的英文字母。
 //        输入描述：输入数据一个 字符串，包括字母，数字 输出描述：
@@ -9,8 +10,9 @@ public class Test1_8 {
 //        String str = "Have you evre gone shopping and";
 //        System.out.println(findThreeTimeChar(str));
 
-//        Find f = new Find();
-//        System.out.println(f.findMan());;
+        Find f = new Find();
+
+        f.findMan();
     }
 
     public static char findThreeTimeChar(String str){
@@ -37,32 +39,65 @@ public class Test1_8 {
 
 //class Find {
 //    public static int findMan(){
-//        int[] arr = new int[]{0,1,2,3,4,5,6,7,8,9};//给是个人编号
-//        int num = 1; //当num=3时，此时的人出局
-//        int isout = 1; //0代表还在圈子里 ，1代表不在圈子里
-//        int m = arr.length;
-//        for(int i = 0; i < m; i++){
-//            int n = arr[i];
-//            if(num == 3){
-//                isout = 0;
-//                /*
-//                if(isout == 0){
-//                    num = 1;
-//                }else{
-//                    m--;
-//                    continue;
-//                }
-//                */
-//            }else{
+//        int[] arr = new int[]{0,0,0,0,0,0,0,0,0,0};//给是个人编号
+//        int num = 0; //当num=3时，此时的人出局
+//         //0代表还在圈子里 ，1代表不在圈子里
+//        int i = 0;
+//        int m = 10;
+//        while(m > 1){
+//            if(arr[i] == 0){
 //                num++;
+//                if(num == 3){
+//                    num = 0;
+//                    arr[i] = 1;
+//                    m--;
+//                }
 //            }
-//            if(m == 1){
-//                return n;
-//            }
-//            if(i == m-1){
+//            i++;
+//            if(i == arr.length){
 //                i = 0;
+//            }
+//        }
+//        for(int a = 0; a < arr.length; a++){
+//            if(arr[a] == 0){
+//                return a+1;
 //            }
 //        }
 //        return 0;
 //    }
 //}
+
+//4、拓展
+class Find {
+    public static void findMan(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入排成一圈的人数：");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = 0;  //0代表还在圈子里 ，1代表不在圈子里
+        }
+        int count = n;
+        int num = 0;
+        int index = 0;
+        while(count > 1){
+            if(arr[index] == 0){
+                num++;
+                if(num == 3){
+                    num = 0;
+                    arr[index] = 1;
+                    count--;
+                }
+            }
+            index++;
+            if(index == n){
+                index = 0;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                System.out.println("最初排在第"+(i+1)+"的人留下了。");
+            }
+        }
+    }
+}
